1efeca77848f011fc38cfb066918eca3a152363d
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-11-29 16:33:12 +0530

    Initial commit

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,64 @@
+class KittensController < ApplicationController
+    include ApplicationHelper
+
+    def index
+        @kittens = Kitten.all
+
+        respond_to do |format|
+          format.html 
+          format.json { render json: @kittens }
+        end
+    end
+
+    def show 
+        @kitten = Kitten.find(params[:id])
+
+        respond_to do |format|
+            format.html 
+            format.json { render json: @kitten }
+        end
+    end
+
+    def new
+        @kitten = Kitten.new
+    end
+
+    def create
+        @kitten = Kitten.new(kitten_params)
+        if @kitten.save
+            flash[:success] = "Kitten created!"
+            redirect_to kittens_path
+        else
+            flash.now[:danger] = "Kitten not created!"
+            render 'new'
+        end
+    end
+
+    def edit
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def update
+        @kitten = Kitten.find(params[:id])
+        if @kitten.update(kitten_params)
+            flash[:success] = "Kitten updated!"
+            redirect_to kitten_path
+        else
+            flash.now[:danger] = "Kitten not updated!"
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @kitten = Kitten.find(params[:id])
+        @kitten.destroy
+        flash[:success] = "Kitten deleted!"
+        redirect_to root_path
+    end
+
+    private
+
+    def kitten_params
+        params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,5 @@
+module ApplicationHelper
+  def render_flash_messages
+    render 'layouts/flash_messages'
+  end
+end
